0d3d61c1401759ccb5c7b2c13e5da93b1b166f06
Shivangi Parashar <meet2shivi@gmail.com>
2021-01-01 19:21:12 +0530

    Update A1_Q1.sh

--- A1_Q1.sh
+++ A1_Q1.sh
@@ -1,5 +1,6 @@
 #! /bin/bash 
 #Shivangi Parashar
+#Write a script which displays “Good morning”, “Good afternoon” or “Good evening”, on the monitor, depending on the system time of running the script. 
 hour=`date +%H`
 
 if [ $hour -lt 12 ]; then
