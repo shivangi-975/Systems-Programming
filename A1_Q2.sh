8fc193bd218d5862b504b9d41596e44a3139847c
Shivangi Parashar <meet2shivi@gmail.com>
2021-01-01 19:22:01 +0530

    Update A1_Q2.sh

--- A1_Q2.sh
+++ A1_Q2.sh
@@ -1,5 +1,8 @@
 #! /bin/bash 
 #Shivangi Parashar
+#Write a bash script which reports the number of regular files in a given directory, with a possible filter on the file extension:
+
+
 for extensions  in jpg png gif tar pdf sh txt ; do 
   file=( *."$extensions" )
  
